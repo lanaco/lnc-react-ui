dd1fb37cfac287942c3dfd476f8e4d1c6d6f835a
Nebojša Kostić <nebojsa.kostic@lanaco.com>
2021-01-28 16:07:10 +0100

    buttons_fix

--- src/IconButton/index.js
+++ src/IconButton/index.js
@@ -10,20 +10,22 @@ const IconButton = (props) => {
     props.onClick(props.id, e.target.value);
   };
 
-    return (
-      <div className={`${styles.containerIconButton} ${props.className}`}>
-        <label className={styles.labelIconButton}></label>
-        <button
-          onClick={handleOnClick}
-          className={(props.inputCssClass) ? [styles.buttonIconIconButton, props.inputCssClass].join(" ") : styles.buttonIconIconButton}
-          disabled={props.disabled}
-          title={props.tooltipText}
-        >
+  return (
+    <div className={baseStyles.baseContainer}>
+      <label className={props.labelCssClass? [baseStyles.baseLabel, props.labelCssClass].join(" ") : baseStyles.baseLabel}></label>
+      <button
+        onClick={handleOnClick}
+        className={(props.inputCssClass) ? [styles.buttonIconIconButton, props.inputCssClass].join(" ") : styles.buttonIconIconButton}
+        disabled={props.disabled}
+        title={props.tooltipText}
+      >
+        <span>
           <i className={props.iconClassName}></i>
-        </button>
-        <div className={props.errorTextCssClass? [baseStyles.baseErrorText, props.errorTextCssClass].join(" ") : baseStyles.baseErrorText}>{props.errorText}</div>
+        </span>
+      </button>
+      <div className={props.errorTextCssClass ? [baseStyles.baseErrorText, props.errorTextCssClass].join(" ") : baseStyles.baseErrorText}>{props.errorText}</div>
     </div>
-    );
+  );
 };
 
 export default IconButton;
