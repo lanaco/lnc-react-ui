5f7fd549ad172c368cc5709fc49c076dcb6740fa
aleksandar mitric <aleksandarmitric91@hotmail.com>
2021-02-18 13:54:26 +0100

    Error fixes

--- src/Base/BaseContainer.js
+++ src/Base/BaseContainer.js
@@ -1,28 +1,52 @@
 import React from "react";
-import "../../dist/index.css";
 import baseStyles from "../Base/styles.module.css";
 
 const BaseContainer = (props) => {
-
-    return (
-        <div className={props.useSideLabel ? baseStyles.baseContainer : baseStyles.baseContainer}>
-            <label className={props.labelCssClass ? [baseStyles.baseLabel, props.labelCssClass].join(" ") : baseStyles.baseLabel}>
-                {props.label}
-                {props.required ? "*" : ""}
-            </label>
-            {props.useSideLabel ?
-                <div>
-                    {props.children}
-                    <div className={props.errorTextCssClass ? [baseStyles.baseErrorText, props.errorTextCssClass].join(" ") : baseStyles.baseErrorText}>{props.errorText}</div>
-                </div>
-                :
-                <React.Fragment>
-                    {props.children}
-                    <div className={props.errorTextCssClass ? [baseStyles.baseErrorText, props.errorTextCssClass].join(" ") : baseStyles.baseErrorText}>{props.errorText}</div>
-                </React.Fragment>
+  return (
+    <div
+      className={
+        props.useSideLabel ? baseStyles.baseContainer : baseStyles.baseContainer
+      }
+    >
+      <label
+        className={
+          props.labelCssClass
+            ? [baseStyles.baseLabel, props.labelCssClass].join(" ")
+            : baseStyles.baseLabel
+        }
+      >
+        {props.label}
+        {props.required ? "*" : ""}
+      </label>
+      {props.useSideLabel ? (
+        <div>
+          {props.children}
+          <div
+            className={
+              props.errorTextCssClass
+                ? [baseStyles.baseErrorText, props.errorTextCssClass].join(" ")
+                : baseStyles.baseErrorText
             }
+          >
+            {props.errorText}
+          </div>
         </div>
-    );
+      ) : (
+        <React.Fragment>
+          {props.children}
+          <div
+            className={
+              props.errorTextCssClass
+                ? [baseStyles.baseErrorText, props.errorTextCssClass].join(" ")
+                : baseStyles.baseErrorText
+            }
+          >
+            {props.errorText}
+          </div>
+        </React.Fragment>
+      )}
+    </div>
+  );
 };
 
 export default BaseContainer;
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "index.js",
-    path: path.resolve(__dirname, "mojtest"),
+    path: path.resolve(__dirname, "dist"),
   },
   module: {
     rules: [
