4305e7bf1c5ffb7f50a9aba415f1df9889f64585
Nebojša Kostić <nebojsa.kostic@lanaco.com>
2021-03-23 16:11:47 +0100

    colorBlender

--- src/Button/index.js
+++ src/Button/index.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import BaseContainer from "../Base/BaseContainer";
+import { getDarkerColor, isColorDark } from "../Base/ColorBlender";
 import baseStyles from "../Base/styles.module.css";
 import styles from "./styles.module.css";
 
 const Button = (props) => {
+
+  const [hover, setHover] = useState(false);
 
   const handleOnClick = (e) => {
     if (props.preventDefault) {
@@ -18,9 +21,51 @@ const Button = (props) => {
     iconClassName = props.iconClassName.replace("-", "_");
   }
 
+  if (props.accentColor) {
+
+    const style = {
+      backgroundColor: props.accentColor,
+      color: isColorDark(props.accentColor) ? "white" : "black"
+    };
+
+    const styleForHover = {
+      backgroundColor: getDarkerColor(props.accentColor, 0.2);
+    }
+
+    return (
+      <BaseContainer {...props} label=" ">
+        <button
+          onMouseOver={() => setHover(true)}
+          onClick={handleOnClick}
+          className={
+            props.inputCssClass
+              ? [styles.standardInputButton, props.inputCssClass].join(" ")
+              : styles.standardInputButton
+          }
+          disabled={props.disabled}
+          title={props.tooltipText}
+          style={hover ? styleForHover : style}
+        >
+          <span>
+            {props.label && props.label !== "" ? <span>{props.label}</span> : ""}&nbsp;
+            {iconClassName !== "" ? (
+              <i
+                className={[baseStyles.lnc, baseStyles[iconClassName]].join(" ")}
+              ></i>
+            ) : (
+              ""
+            )}
+          </span>
+        </button>
+      </BaseContainer>
+    );
+
+  }
+
   return (
     <BaseContainer {...props} label=" ">
       <button
+        onMouseOver={() => setHover(true)}
         onClick={handleOnClick}
         className={
           props.inputCssClass
@@ -37,8 +82,8 @@ const Button = (props) => {
               className={[baseStyles.lnc, baseStyles[iconClassName]].join(" ")}
             ></i>
           ) : (
-              ""
-            )}
+            ""
+          )}
         </span>
       </button>
     </BaseContainer>
