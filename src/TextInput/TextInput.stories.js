f29d23c6a3317d02a4604331cdf5b04ff042d572
Nemanja Dragovic <nemanja.dragovic@lanaco.com>
2021-07-08 13:33:29 +0200

    fix

---
+++ src/TextInput/TextInput.stories.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import TextInput from '.';
+
+export default {
+  title: 'TextInput',
+  component: TextInput,
+  // argTypes: {
+  //   backgroundColor: { control: 'color' },
+  // },
+};
+
+const Template = (args) => <TextInput {...args} />;
+
+export const Primary = Template.bind({});
+// Primary.args = {
+//   primary: true,
+//   label: 'Button',
+//   aa: 'a'
+// };
+
+// export const Secondary = Template.bind({});
+// Secondary.args = {
+//   label: 'Button',
+// };
+
+// export const Large = Template.bind({});
+// Large.args = {
+//   size: 'large',
+//   label: 'Button',
+// };
+
+// export const Small = Template.bind({});
+// Small.args = {
+//   size: 'small',
+//   label: 'Button',
+// };
