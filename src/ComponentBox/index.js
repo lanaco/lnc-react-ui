6d2422443df69d50df74b06714afa1baa2360962
Vladan Racic <vladanracic@lanaco.com>
2021-08-06 14:11:21 +0200

    component box

--- src/ComponentBox/index.js
+++ src/ComponentBox/index.js
@@ -2,114 +2,212 @@ import React, { useEffect } from "react";
 import styles from "./styles.module.css";
 import Button from "../Button/index.js";
 
-function ComponentBox(props) {
-  const emptyFunc = () => {};
+import PropTypes from "prop-types";
+import styled from "@emotion/styled";
+import theme from "../_utils/theme";
 
+const Container = styled.div((props) => ({
+  position: "fixed",
+  alignItems: "center",
+  background: "rgba(0, 0, 0, 0.5)",
+  width: "100%",
+  height: "100%",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "center",
+  zIndex: 10000,
+}));
+
+const Modal = styled.div((props) => ({
+  background: "white",
+  transition: "1.1s ease-out",
+  visibility: "visible",
+  position: "relative",
+  width: "70%",
+}));
+
+const Header = styled.div((props) => ({
+  padding: "5px",
+  display: "flex",
+}));
+
+const Title = styled.div((props) => ({
+  fontSize: "12px",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const CloseButton = styled.div((props) => ({
+  marginLeft: "auto",
+}));
+
+const Content = styled.div((props) => ({
+  borderTop: `0.125rem solid ${props.}`,
+  padding: "5px",
+}));
+
+function ComponentBox(props) {
   const {
-    basic = false,
+    id,
+    onClose,
     open,
-    zIndex = 10000,
-    componentBoxList = [],
-    addComponentBox = emptyFunc,
-    closeComponentBox = emptyFunc,
+    zIndex,
+    header,
+    className,
+    theme,
+    size,
+    color,
   } = props;
 
-  let componentBox = !basic
-    ? componentBoxList.find((x) => {
-        return x.ID === props.id;
-      })
-    : undefined;
+  let themeProps = { theme, size, color}
 
-  useEffect(() => {
-    if (!basic) addComponentBox(props.id);
-  });
+  return (
+    <Container {...themeProps}>
+      <Modal {...themeProps}>
+        <Header {...themeProps}>
+          <Title {...themeProps}>Header</Title>
+          <CloseButton {...themeProps}>
+            <Button icon={"times"} iconStyle={"solid"} onClick={() => {}} />
+          </CloseButton>
+        </Header>
+        <Content {...themeProps}>Content</Content>
+      </Modal>
+    </Container>
+  );
 
-  const handleDialogClose = () => {
-    props.handleDialogClose !== undefined
-      ? props.handleDialogClose()
-      : closeComponentBox(props.id);
-  };
+  // let componentBox = !basic
+  //   ? componentBoxList.find((x) => {
+  //       return x.ID === props.id;
+  //     })
+  //   : undefined;
 
-  const handleClickOutsideModal = (e) => {
-    if (props.closeModalOnOutsideClick && e.target.className === styles.modal) {
-      handleDialogClose();
-    }
-  };
+  // useEffect(() => {
+  //   if (!basic) addComponentBox(props.id);
+  // });
 
-  const getModalContentClass = () => {
-    let modalContentClass =
-      props.size === "small"
-        ? [
-            styles.componentBoxModalContent,
-            styles.smallModal,
-            styles.padding,
-          ].join(" ")
-        : props.size === "large"
-        ? [
-            styles.componentBoxModalContent,
-            styles.largeModal,
-            styles.padding,
-          ].join(" ")
-        : [
-            styles.componentBoxModalContent,
-            styles.mediumModal,
-            styles.padding,
-          ].join(" ");
+  // const handleDialogClose = () => {
+  //   props.handleDialogClose !== undefined
+  //     ? props.handleDialogClose()
+  //     : closeComponentBox(props.id);
+  // };
 
-    if (props.withoutPadding) {
-      modalContentClass =
-        props.size === "small"
-          ? [styles.componentBoxModalContent, styles.smallModal].join(" ")
-          : props.size === "large"
-          ? [styles.componentBoxModalContent, styles.largeModal].join(" ")
-          : [styles.componentBoxModalContent, styles.mediumModal].join(" ");
-    }
-    return modalContentClass;
-  };
+  // const handleClickOutsideModal = (e) => {
+  //   if (props.closeModalOnOutsideClick && e.target.className === styles.modal) {
+  //     handleDialogClose();
+  //   }
+  // };
 
-  const renderComponentBox = () => {
-    let index = basic ? zIndex : componentBox.zIndex;
+  // const getModalContentClass = () => {
+  //   let modalContentClass =
+  //     props.size === "small"
+  //       ? [
+  //           styles.componentBoxModalContent,
+  //           styles.smallModal,
+  //           styles.padding,
+  //         ].join(" ")
+  //       : props.size === "large"
+  //       ? [
+  //           styles.componentBoxModalContent,
+  //           styles.largeModal,
+  //           styles.padding,
+  //         ].join(" ")
+  //       : [
+  //           styles.componentBoxModalContent,
+  //           styles.mediumModal,
+  //           styles.padding,
+  //         ].join(" ");
 
-    return (
-      <div
-        className={styles.modal}
-        onClick={handleClickOutsideModal}
-        style={{ zIndex: index }}
-      >
-        <section className={getModalContentClass()}>
-          <div className={styles.titleLine}>
-            <div className={styles.table}>
-              <div className={styles.tableCell}>
-                {props.dontShowTitle ? "" : props.title}
-              </div>
-            </div>
-            <div className={styles.closeButton}>
-              {props.dontShowCloseButton ? (
-                ""
-              ) : (
-                <Button
-                  disabled={props.disabled}
-                  icon={"times"}
-                  iconStyle={"solid"}
-                  onClick={handleDialogClose}
-                  // iconCssClass={styles.closeButtonPadding}
-                  // inputCssClass={styles.closeButtonPadding}
-                />
-              )}
-            </div>
-          </div>
-          <div>{props.children}</div>
-        </section>
-      </div>
-    );
-  };
+  //   if (props.withoutPadding) {
+  //     modalContentClass =
+  //       props.size === "small"
+  //         ? [styles.componentBoxModalContent, styles.smallModal].join(" ")
+  //         : props.size === "large"
+  //         ? [styles.componentBoxModalContent, styles.largeModal].join(" ")
+  //         : [styles.componentBoxModalContent, styles.mediumModal].join(" ");
+  //   }
+  //   return modalContentClass;
+  // };
 
-  if (
-    (basic === true && open !== undefined && open === true) ||
-    (componentBox !== undefined && componentBox.Open)
-  )
-    return renderComponentBox();
-  else return <React.Fragment />;
+  // const renderComponentBox = () => {
+  //   let index = basic ? zIndex : componentBox.zIndex;
+
+  //   return (
+  //     <div
+  //       className={styles.modal}
+  //       onClick={handleClickOutsideModal}
+  //       style={{ zIndex: index }}
+  //     >
+  //       <section className={getModalContentClass()}>
+  //         <div className={styles.titleLine}>
+  //           <div className={styles.table}>
+  //             <div className={styles.tableCell}>
+  //               {props.dontShowTitle ? "" : props.title}
+  //             </div>
+  //           </div>
+  //           <div className={styles.closeButton}>
+  //             {props.dontShowCloseButton ? (
+  //               ""
+  //             ) : (
+  //               <Button
+  //                 disabled={props.disabled}
+  //                 icon={"times"}
+  //                 iconStyle={"solid"}
+  //                 onClick={handleDialogClose}
+  //                 // iconCssClass={styles.closeButtonPadding}
+  //                 // inputCssClass={styles.closeButtonPadding}
+  //               />
+  //             )}
+  //           </div>
+  //         </div>
+  //         <div>{props.children}</div>
+  //       </section>
+  //     </div>
+  //   );
+  // };
+
+  // if (
+  //   (basic === true && open !== undefined && open === true) ||
+  //   (componentBox !== undefined && componentBox.Open)
+  // )
+  //   return renderComponentBox();
+  // else return <React.Fragment />;
 }
+
+ComponentBox.defaultProps = {
+  id: "",
+  open: false,
+  onClose: () => {},
+  className: "",
+  header: "",
+  zIndex: 1000,
+  size: "small",
+  color: "primary",
+  theme: theme,
+  clickOutsideToClose: false,
+};
+
+ComponentBox.propTypes = {
+  theme: PropTypes.object.isRequired,
+  id: PropTypes.string,
+  onClose: PropTypes.func,
+  className: PropTypes.string,
+  header: PropTypes.string,
+  zIndex: PropTypes.number,
+  open: PropTypes.bool,
+  size: PropTypes.oneOf(["small", "medium", "large"]),
+  color: PropTypes.oneOf([
+    "primary",
+    "secondary",
+    "success",
+    "error",
+    "warning",
+    "gray",
+    "background",
+  ]),
+};
 
 export default ComponentBox;
