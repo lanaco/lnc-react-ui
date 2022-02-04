a769e28e40eadfec6b343546463108fceb84f153
Srdjan Kovacevic <srdjan.kovacevic@lanaco.com>
2022-02-04 11:25:25 +0100

    css fixes

--- src/ItemCounter/ItemCounter.stories.js
+++ src/ItemCounter/ItemCounter.stories.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ItemCounter from "./index";
+import theme from "../_utils/theme";
 
 export default {
   title: "Item counter",
@@ -11,11 +12,27 @@ const Template = (args) => <ItemCounter {...args} />;
 export const Normal = Template.bind({});
 Normal.args = {
   Items: [
-    { key: "Item1", number: 2, type: { id: 1, code: "Success" } },
-    { key: "Item2", number: 34, type: { id: 2, code: "Danger" } },
-    { key: "Item3", number: 314, type: { id: 3, code: "Warning" } },
+    {
+      key: "Item1",
+      number: 2,
+      type: { id: 1, code: "Success" },
+      description: "Project",
+    },
+    {
+      key: "Item2",
+      number: 34,
+      type: { id: 2, code: "Danger" },
+      description: "Resources",
+    },
+    {
+      key: "Item3",
+      number: 314,
+      type: { id: 3, code: "Warning" },
+      description: "Days",
+    },
   ],
   size: "small",
   color: "primary",
   disabled: false,
+  theme: theme,
 };
--- src/ItemCounter/index.js
+++ src/ItemCounter/index.js
@@ -1,9 +1,29 @@
 import React from "react";
 import styles from "./styles.module.css";
 import ItemCounterTypes from "./ItemCounterTypes";
+import styled from "@emotion/styled";
+
+const ContainerWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  justify-content: flex-start;
+  border-radius: 2px;
+`;
+
+const Container = styled.div`
+  width: 100%;
+  background-color: #ebf9ff;
+  padding: 6px;
+  border: 2px solid white;
+  width: fit-content;
+  border-radius: 8%;
+  font-family: "Ubuntu";
+  font-size: "0.70rem";
+`;
 
 const ItemCounter = (props) => {
-  const { Items = [], ContainerWrapperClassName = "" } = props;
+  const { Items = [] } = props;
 
   const getClassForType = (typeCode) => {
     if (typeCode === ItemCounterTypes.Success.code) return styles.success;
@@ -13,24 +33,18 @@ const ItemCounter = (props) => {
 
   if (Items) {
     return (
-      <div
-        className={
-          ContainerWrapperClassName
-            ? [styles.ContainerWrapper, ContainerWrapperClassName].join(" ")
-            : styles.ContainerWrapper
-        }
-      >
+      <ContainerWrapper {...props.theme}>
         {Items.map((item, i) => (
-          <div className={styles.container} key={i}>
+          <Container key={i}>
             <div
               className={styles.number + " " + getClassForType(item.type.code)}
             >
               {item.number}
             </div>
             <div className={styles.number}># of {item.description}</div>
-          </div>
+          </Container>
         ))}
-      </div>
+      </ContainerWrapper>
     );
   } else {
     return <div />;
