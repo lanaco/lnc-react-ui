0f25f3721c6b8942dcdd161ace4ea941fd21fd05
Vladan Racic <vladanracic@lanaco.com>
2022-04-01 14:50:28 +0200

    analytical table

--- src/Data display/AnalyticalTable/components/AnalyticalTableGroupRow.js
+++ src/Data display/AnalyticalTable/components/AnalyticalTableGroupRow.js
@@ -6,10 +6,8 @@ import { usePrevious } from "react-use";
 import { isEqual } from "lodash";
 
 const Row = styled.tr`
-  opacity: ${(props) => props.opacity};
-  border: 1px solid gray;
   border-radius: 2px;
-  background-color: gray;
+  background-color: ${(props) => props.bgColor};
   cursor: pointer;
   display: ${(props) => (props.show === false ? "none" : "default")};
 `;
@@ -17,37 +15,35 @@ const Row = styled.tr`
 const Cell = styled.td`
   margin: 3px;
   padding: 6px;
-  color: white;
   font-family: Arial;
   font-size: 12px;
-  font-weight: bold;
   border: 1px solid white;
 `;
 
 const LeafCell = styled.td`
   padding: 6px;
-  font-weight: bold;
   text-align: ${(props) => (props.isNumber ? "right" : "left")};
-  border: 1px solid black;
 `;
 
-const AnalyticalTableGroupRow = ({
-  node,
-  depth,
-  isLeaf,
-  getData,
-  ExpandCollapseGroup,
-  show = true,
-  Columns,
-  GroupByFields,
-}) => {
+const AnalyticalTableGroupRow = (props) => {
   //
+  const {
+    Node,
+    Depth,
+    IsLeaf,
+    GetData,
+    ExpandCollapseGroup,
+    Show = true,
+    Columns,
+    GroupByFields,
+  } = props;
+
   const [leafs, setLeafs] = useState([]);
   const prevGroupBy = usePrevious(GroupByFields);
 
   useEffect(() => {
-    if (!show) setLeafs([]);
-  }, [show]);
+    if (!Show) setLeafs([]);
+  }, [Show]);
 
   useEffect(() => {
     if (!isEqual(prevGroupBy, GroupByFields)) setLeafs([]);
@@ -55,14 +51,22 @@ const AnalyticalTableGroupRow = ({
 
   //============================================================================
 
+  const getBgColorByLevel = () => {
+    if (Depth === 0) return theme.palette.gray[600];
+    if (Depth === 1) return theme.palette.gray[400];
+    if (Depth === 2) return theme.palette.gray[200];
+
+    return "white";
+  };
+
   const getOpacityByLevel = () => {
-    return 1 - depth * 0.25;
+    return 1 - Depth * 0.25;
   };
 
   const renderEmptyGroupedCells = () => {
     var cells = [];
 
-    for (let i = 0; i < depth; i++) {
+    for (let i = 0; i < Depth; i++) {
       cells.push(<Cell />);
     }
 
@@ -72,7 +76,7 @@ const AnalyticalTableGroupRow = ({
   const renderEmptyLeafCells = () => {
     var cells = [];
 
-    for (let i = 0; i < depth + 1; i++) {
+    for (let i = 0; i < Depth + 1; i++) {
       cells.push(<td />);
     }
 
@@ -94,15 +98,15 @@ const AnalyticalTableGroupRow = ({
   //============================================================================
 
   const handleClick = async () => {
-    if (!isLeaf) ExpandCollapseGroup(depth, node);
+    if (!IsLeaf) ExpandCollapseGroup(Depth, Node);
 
-    if (isLeaf && leafs.length === 0 && getData) {
+    if (IsLeaf && leafs.length === 0 && GetData) {
       //
-      var leafData = await getData(node.parentInfo);
+      var leafData = await GetData(Node.parentInfo);
       setLeafs(leafData);
 
       //
-    } else if (isLeaf && leafs.length !== 0) setLeafs([]);
+    } else if (IsLeaf && leafs.length !== 0) setLeafs([]);
   };
 
   //============================================================================
@@ -118,10 +122,15 @@ const AnalyticalTableGroupRow = ({
 
   return (
     <>
-      <Row show={show} opacity={getOpacityByLevel()} onClick={handleClick}>
+      <Row
+        show={Show}
+        bgColor={getBgColorByLevel()}
+        opacity={getOpacityByLevel()}
+        onClick={handleClick}
+      >
         {renderEmptyGroupedCells()}
 
-        <Cell colSpan={4}>{node.value}</Cell>
+        <Cell colSpan={4}>{Node.value}</Cell>
       </Row>
 
       {renderLeafs()}
