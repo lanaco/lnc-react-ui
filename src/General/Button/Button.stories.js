417b18de51f571d46d82bdf7cd15b2b52b4f60cf
Vladan Racic <vladanracic@lanaco.com>
2022-06-09 11:23:06 +0200

    button and icon button

--- src/General/Button/Button.stories.js
+++ src/General/Button/Button.stories.js
@@ -9,40 +9,24 @@ export default {
 };
 
 const Template = (args) => (
-  <>
-    <p>
-      <Button {...args} />
-    </p>
-  </>
+  <div style={{ width: "150px" }}>
+    <Button {...args} />
+  </div>
 );
 
 export const JustText = Template.bind({});
 JustText.args = {
-  theme: theme,
   color: "primary",
   text: "Button",
   tooltip: "Button",
   onClick: () => {},
-  size: "medium",
+  size: "small",
   disabled: false,
-  outline: false,
-};
-
-export const JustIcon = Template.bind({});
-JustIcon.args = {
-  color: "primary",
-  tooltip: "Button",
-  onClick: () => {},
-  size: "medium",
-  disabled: false,
-  icon: "user",
-  iconStyle: "solid",
   outline: false,
 };
 
 export const TextAndIconToLeft = Template.bind({});
 TextAndIconToLeft.args = {
-  theme: theme,
   color: "primary",
   text: "Button",
   tooltip: "Button",
@@ -57,7 +41,6 @@ TextAndIconToLeft.args = {
 
 export const TextAndIconToRight = Template.bind({});
 TextAndIconToRight.args = {
-  theme: theme,
   color: "primary",
   text: "Button",
   tooltip: "Button",
@@ -66,6 +49,6 @@ TextAndIconToRight.args = {
   disabled: false,
   icon: "user",
   iconStyle: "solid",
-  iconLocation: "right",
-  outline: false,
+  iconLocation: "left",
+  outline: true,
 };
