d34c662e0dc5263f5e74f9f9069635b1aeaad781
zubacm <zubacm5@gmail.com>
2022-06-15 15:20:31 +0200

    range slider stories

--- src/General/Progress/index.js
+++ src/General/Progress/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from 'react';
 import PropTypes from "prop-types";
 import { useTheme } from "@emotion/react";
 import styled from "@emotion/styled";
