3f1dd60cc67f1f29bcb262607f5ce17f42e62e7a
babics <babics@nomail.com>
2022-07-06 08:43:19 +0200

    Added forwardRef

--- src/Layout/Content/index.js
+++ src/Layout/Content/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Content = (props) => {
+const Content = React.forwardRef((props, ref) => {
   const { children, __TYPE__, ...rest } = props;
 
-  return <main {...rest}>{children}</main>;
-};
+  return (
+    <main ref={ref} {...rest}>
+      {children}
+    </main>
+  );
+});
 
 Content.defaultProps = {
   __TYPE__: "Content",
@@ -13,7 +17,7 @@ Content.defaultProps = {
 
 Content.propTypes = {
   /**
-   * Do not override this property. 
+   * Do not override this property.
    * Should only be used as indicator for type if you are passing custom component.
    */
   __TYPE__: PropTypes.string,
--- src/Layout/Footer/index.js
+++ src/Layout/Footer/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Footer = (props) => {
+const Footer = React.forwardRef((props, ref) => {
   const { children, __TYPE__, ...rest } = props;
 
-  return <footer {...rest}>{children}</footer>;
-};
+  return (
+    <footer ref={ref} {...rest}>
+      {children}
+    </footer>
+  );
+});
 
 Footer.defaultProps = {
   __TYPE__: "Footer",
@@ -13,7 +17,7 @@ Footer.defaultProps = {
 
 Footer.propTypes = {
   /**
-   * Do not override this property. 
+   * Do not override this property.
    * Should only be used as indicator for type if you are passing custom component.
    */
   __TYPE__: PropTypes.string,
