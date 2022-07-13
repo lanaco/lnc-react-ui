73a6d5a336f773839def7458ee80daedd23bbf46
babics <babics@nomail.com>
2022-07-13 14:16:55 +0200

    Fixed story for flexbox.

--- src/Layout/FlexBox/FlexBox.stories.js
+++ src/Layout/FlexBox/FlexBox.stories.js
@@ -6,6 +6,42 @@ export default {
   component: FlexBox,
 };
 
-const Template = (args) => <FlexBox {...args}><div>asa</div><div>masa</div></FlexBox>;
+const Template = (args) => (
+  <FlexBox {...args}>
+    <div
+      style={{
+        backgroundColor: "#00628F",
+        color: "white",
+        padding: "10px",
+        height: "100px",
+      }}
+    >
+      Child with height
+    </div>
+    <div
+      style={{
+        backgroundColor: "#00628F",
+        color: "white",
+        padding: "10px",
+      }}
+    >
+      Child
+    </div>
+    <div
+      style={{
+        backgroundColor: "#00628F",
+        color: "white",
+        padding: "10px",
+      }}
+    >
+      Child
+    </div>
+  </FlexBox>
+);
 
 export const Default = Template.bind({});
+Default.args = {
+  justifyContent: "SpaceEvenly",
+  alignItems: "Center",
+  direction: "Row",
+};
