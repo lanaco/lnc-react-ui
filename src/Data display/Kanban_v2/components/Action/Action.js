23c651019d551758c8e8f5025897059ca12c44e1
Vladan Racic <vladanracic@lanaco.com>
2022-09-20 10:12:44 +0200

    kanban v2

---
+++ src/Data display/Kanban_v2/components/Action/Action.js
@@ -0,0 +1,22 @@
+import React, { forwardRef } from "react";
+import classNames from "classnames";
+import styles from "./Action.module.css";
+
+export const Action = forwardRef(
+  ({ active, className, cursor, style, ...props }, ref) => {
+    return (
+      <button
+        ref={ref}
+        {...props}
+        className={classNames(styles.Action, className)}
+        tabIndex={0}
+        style={{
+          ...style,
+          cursor,
+          "--fill": active?.fill,
+          "--background": active?.background,
+        }}
+      />
+    );
+  }
+);
