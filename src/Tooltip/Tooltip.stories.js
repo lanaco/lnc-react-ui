5053075041f740785994df885981c06a7aeb53c6
nk-lnc <nebojsa.kostic@lanaco.com>
2022-11-02 11:13:21 +0100

    Tooltip_added

--- src/Tooltip/Tooltip.stories.js
+++ src/Tooltip/Tooltip.stories.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Tooltip from ".";
-import SimpleTooltipExample from "./SimpleTooltipExample";
 
 export default {
     title: "Tooltip",
@@ -8,15 +7,41 @@ export default {
     argTypes: {},
 };
 
+const GetTooltipContent = () => {
+    return (<div><h3>TOOLTIP</h3><p>This is simple tooltip content</p></div>);
+}
+
 
 const Template = (args) => (
     <>
         <div id="modal-root"></div>
-        <SimpleTooltipExample {...args} />
+        <div style={{ height: "100vh", width: "100vw", display: "grid", gridTemplateRows: "1fr 1fr", gridTemplateColumns: "1fr 1fr" }}>
+            <Tooltip {...args}>
+                <div style={{ border: "1px solid red" }}>
+                    Hover here to show tooltip
+                </div>
+            </Tooltip>
+            <Tooltip {...args}>
+                <div>
+                    Or here
+                </div>
+            </Tooltip>
+            <Tooltip {...args}>
+                <div>
+                    Or here :)
+                </div>
+            </Tooltip>
+            <Tooltip {...args}>
+                <div>
+                    Or here
+                </div>
+            </Tooltip>
+        </div>
     </>
 );
 
 export const SimpleTooltip = Template.bind({});
 SimpleTooltip.args = {
-    targetID: "modal-root",
+    targetID: "#modal-root",
+    renderContent: GetTooltipContent
 };
--- src/Tooltip/index.js
+++ src/Tooltip/index.js
@@ -1,31 +1,97 @@
+import styled from "@emotion/styled";
 import PropTypes from "prop-types";
-import { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from 'react-dom';
 import "../Base/fontawesome/css/fontawesome.css";
 
 const Tooltip = props => {
-    const { targetID, show, } = props;
+    const { targetID, renderContent } = props;
 
-    const target = document.querySelector(targetID);
+    const [show, setShow] = useState(false);
+    const [style, setStyle] = useState("position: absolute;");
+
+    const target = (targetID.startsWith('#')) ? document.querySelector(targetID) : document.querySelector("#" + targetID);
 
     const el = document.createElement('div');
-    el.style = "position: absolute; left: 200px; top: 200px; background-color: yellow;";
+    el.style = style;
 
     useEffect(() => {
-        target.innerHTML = "";
-        setTimeout(() => {
-            if (show) {
-                target.appendChild(el);
-            }
-        }, 300);
-    }, [show]);
+        if (show && target !== null) {
+            target.appendChild(el);
+        }
+    }, [targetID, show]);
 
-    return ReactDOM.createPortal(props.children, el);
+    const TooltipBoundary = styled.div`
+    height: min-content;
+    width: min-content;
+    `;
+
+    const CalculateStyle = (mouseX, mouseY) => {
+        if (mouseX <= window.innerWidth / 2 && mouseY <= window.innerHeight / 2) {
+            return "position: absolute; left: " + (mouseX - 10) + "px; top: " + (mouseY - 10) +
+                "px; width: auto; height: auto; box-shadow: 0 0 6px #bebebe; border-radius: 0.175rem; background-color: white; " +
+                "overflow: auto; + max-height: " + (window.innerHeight - 20) + "px; z-index: 999;";
+        }
+        else if (mouseX > window.innerWidth / 2 && mouseY <= window.innerHeight / 2) {
+            return "position: absolute; right: " + (window.innerWidth - mouseX - 10) + "px; top: " + (mouseY - 10) +
+                "px; width: auto; height: auto; box-shadow: 0 0 6px #bebebe; border-radius: 0.175rem; background-color: white; " +
+                "overflow: auto; + max-height: " + (window.innerHeight - 20) + "px; z-index: 999;";
+        }
+        else if (mouseX <= window.innerWidth / 2 && mouseY > window.innerHeight / 2) {
+            return "position: absolute; left: " + (mouseX - 10) + "px; bottom: " + (window.innerHeight - mouseY - 10) +
+                "px; width: auto; height: auto; box-shadow: 0 0 6px #bebebe; border-radius: 0.175rem; background-color: white; " +
+                "overflow: auto; + max-height: " + (window.innerHeight - 20) + "px; z-index: 999;";
+        }
+        else if (mouseX > window.innerWidth / 2 && mouseY > window.innerHeight / 2) {
+            return "position: absolute; right: " + (window.innerWidth - mouseX - 10) + "px; bottom: " + (window.innerHeight - mouseY - 10) +
+                "px; width: auto; height: auto; box-shadow: 0 0 6px #bebebe; border-radius: 0.175rem; background-color: white; " +
+                "overflow: auto; + max-height: " + (window.innerHeight - 20) + "px; z-index: 999;";
+        }
+    }
+
+    const renderContentWithBounds = () => {
+        return (
+            <div onMouseLeave={event => {
+                event.stopPropagation();
+                target.innerHTML = "";
+                setStyle("position: absolute;");
+                setShow(false);
+            }}>
+                {renderContent()}
+            </div>);
+    }
+
+    return (
+        <>
+            {ReactDOM.createPortal(renderContentWithBounds(), el)}
+            <TooltipBoundary
+                onMouseOver={event => {
+                    event.stopPropagation();
+                    if (show === false) {
+                        setStyle(CalculateStyle(event.clientX, event.clientY));
+                        setShow(true);
+                    }
+                }}
+                onMouseLeave={event => {
+                    event.stopPropagation();
+                    target.innerHTML = "";
+                    setStyle("position: absolute;");
+                    setShow(false);
+                }}>
+                {props.children}
+            </TooltipBoundary>
+        </>
+    );
 }
+
+Tooltip.defaultProps = {
+    targetID: "",
+    renderContent: () => { },
+};
 
 Tooltip.propTypes = {
     targetID: PropTypes.string,
-    show: PropTypes.bool
+    renderContent: PropTypes.func
 };
 
 export default Tooltip;
