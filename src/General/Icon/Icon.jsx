422c0be7ba68e10834e057765af867e9904e48f9
Vladan Racic <85110688+vladanracic@users.noreply.github.com>
2024-11-26 11:07:18 +0100

    removed css

--- src/General/Icon/Icon.jsx
+++ src/General/Icon/Icon.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable react/display-name */
 import { forwardRef } from "react";
-import "../../Base/fontawesome/css/fontawesome.css";
 import PropTypes from "prop-types";
 import styled from "@emotion/styled";
 import { useTheme } from "@emotion/react";
