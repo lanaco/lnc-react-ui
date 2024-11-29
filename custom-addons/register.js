65a9fe4f6bb0205c36d7251b6a7d3b79b11a4e1e
Vladan Racic <85110688+vladanracic@users.noreply.github.com>
2024-11-29 16:32:16 +0100

    fixes

--- custom-addons/register.js
+++ custom-addons/register.js
@@ -1,14 +1,13 @@
+import React from "react";
+import { addons, types } from "@storybook/addons";
 
-import React from 'react';
-import { addons, types } from '@storybook/addons';
+import { ThemeSelector } from "./ThemeSelector";
 
-import { ThemeSelector } from './ThemeSelector';
-
-const ADDON_ID = 'smyaddon';
+const ADDON_ID = "smyaddon";
 
 addons.register(ADDON_ID, () => {
   addons.add(ADDON_ID, {
-    title: 'Theme',
+    title: "Theme",
     type: types.TOOL,
     match: ({ viewMode }) => !!(viewMode && viewMode.match(/^(story|docs)$/)),
     render: () => <ThemeSelector />,
