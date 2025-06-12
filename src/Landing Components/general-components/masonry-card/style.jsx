93e8b1297773dabae2c7aad4056dee9f3a4db63e
Aleksandar Radic <77508463+Rad1c@users.noreply.github.com>
2025-06-12 13:21:34 +0200

    Masonry card landing page section (#430)
    * Added masonay card landing page section
    
    * version
    
    * fixes
    
    ---------
    
    Co-authored-by: Aleksandar Radc <aleksandar.radic@lanaco.com>

---
+++ src/Landing Components/general-components/masonry-card/style.jsx
@@ -0,0 +1,104 @@
+import styled from "@emotion/styled";
+
+import {
+  linearGradientAnimation,
+  truncateTextInRows,
+} from "../../../_utils/utils";
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  border-radius: 0.75rem;
+  position: relative;
+  box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.1), 0px 1px 2px rgba(0, 0, 0, 0.06);
+
+  grid-column: ${(p) => p.position?.columnStart || "1"} /
+    ${(p) => p.position?.columnEnd || "6"};
+  grid-row: ${(p) => p.position?.rowStart || "1"} /
+    ${(p) => p.position?.rowEnd || "1"};
+
+  & .wrapper__image {
+    height: 100%;
+    width: 100%;
+    object-fit: cover;
+    border-radius: 0.75rem;
+  }
+
+  & .wrapper__image--skeleton {
+    width: 24.5rem;
+    height: 30rem;
+    border-radius: 0.75rem;
+    background-color: ${linearGradientAnimation("-90deg")};
+  }
+
+  & .wrapper__text {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    padding: 1rem;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    gap: 0.75rem;
+
+    & .text__title {
+      color: var(--white, #ffffff);
+      font-size: 1rem;
+      font-style: normal;
+      font-weight: 500;
+      line-height: 1.5rem;
+      ${truncateTextInRows(2)}
+    }
+
+    & .text__tag {
+      background-color: var(--warning-600, #d97706);
+      color: var(--white, #ffffff);
+      font-size: 0.875rem;
+      font-style: normal;
+      font-weight: 500;
+      line-height: 1.25rem;
+      padding: 0.25rem 0.5rem;
+
+      border-radius: 0.375rem;
+      background: var(--warning-600, #d97706);
+    }
+
+    & .text__description {
+      color: var(--white);
+      font-size: 0.875rem;
+      font-style: normal;
+      font-weight: 400;
+      line-height: 1.25rem;
+      ${truncateTextInRows(2)}
+    }
+
+    & .text__action {
+      background: var(--white);
+      color: var(--black);
+
+      &:hover {
+        background: var(--primary-500, #f59e0b);
+        color: var(--white);
+      }
+    }
+
+    & .text__title--skeleton {
+      background-color: ${linearGradientAnimation("-90deg")};
+      height: 1rem;
+      width: 9rem;
+    }
+
+    & .text__description--skeleton {
+      background-color: ${linearGradientAnimation("-90deg")};
+      height: 1rem;
+      width: 12rem;
+    }
+
+    & .text__action--skeleton {
+      background-color: ${linearGradientAnimation("-90deg")};
+      height: 2rem;
+      width: 9rem;
+    }
+  }
+`;
