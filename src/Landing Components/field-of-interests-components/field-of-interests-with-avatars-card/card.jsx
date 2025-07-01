d6a2b1d2a312a7d65545481a1fb04f354da32bc2
zubacm <46137823+zubacm@users.noreply.github.com>
2025-07-01 08:43:58 +0200

    User/zubacm/fix text (#459)
    * fix
    
    * v
    
    ---------
    
    Co-authored-by: Marina Zubac <marina.zubac@lanaco.com>

--- src/Landing Components/field-of-interests-components/field-of-interests-with-avatars-card/card.jsx
+++ src/Landing Components/field-of-interests-components/field-of-interests-with-avatars-card/card.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/display-name */
+/* eslint-disable react/prop-types */
 import { forwardRef } from "react";
 
 import SimpleProductCard from "../../product components/simple-product-card";
