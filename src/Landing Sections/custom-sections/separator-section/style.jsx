f4b0636c97f58f6551ebe1835d3da7f352f35023
Marina Zubac <marina.zubac@lanaco.com>
2025-07-08 10:01:48 +0200

    separator mobile style

--- src/Landing Sections/custom-sections/separator-section/style.jsx
+++ src/Landing Sections/custom-sections/separator-section/style.jsx
@@ -1,7 +1,12 @@
 import styled from "@emotion/styled";
+import { MOBILE_SIZE_PX } from "../../../_utils/consts";
 
 export const Wrapper = styled.div`
-width: 100%;
-height: 0.125rem;
-background-color: var(--gray-95008, rgba(20, 22, 26, 0.08));
+  width: 100%;
+  height: 0.125rem;
+  background-color: var(--gray-95008, rgba(20, 22, 26, 0.08));
+
+  @media (max-width: ${MOBILE_SIZE_PX + "px"}) {
+    height: 0.5rem;
+  }
 `;
