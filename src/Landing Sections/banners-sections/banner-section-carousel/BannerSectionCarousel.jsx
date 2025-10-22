556e2b18ff6da836db5090b9194a37ed6364e217
Milan Brčkalo <brckvlo3@gmail.com>
2025-10-22 11:28:33 +0200

    Redesigned banner section carousel

--- src/Landing Sections/banners-sections/banner-section-carousel/BannerSectionCarousel.jsx
+++ src/Landing Sections/banners-sections/banner-section-carousel/BannerSectionCarousel.jsx
@@ -1,71 +1,95 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable react/display-name */
 import { forwardRef } from "react";
-import { Carousel } from "react-responsive-carousel";
+
+import Carousel from "react-multi-carousel";
+
+import "react-multi-carousel/lib/styles.css";
+
 import PropTypes from "prop-types";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Wrapper, Banner, BannerOverlay } from "./style";
+
 import Button from "../../../General/Button/Button";
-import { isDefinedNotEmptyString } from "../../../_utils/utils";
+import useDetectMobile from "../../../_utils/useDetectMobile";
+import { Container } from "./style";
 
 const BannerSectionCarousel = forwardRef(
-  (
-    { carouselItems = [], onSelectItem = () => {}, onButtonAction = () => {} },
-    ref
-  ) => {
-    return (
-      <Wrapper ref={ref}>
-        <Carousel
-          autoPlay={true}
-          infiniteLoop={true}
-          showArrows={false}
-          showStatus={false}
-          showThumbs={false}
-          transitionTime={500}
-          onClickItem={(idx) => {
-            onSelectItem(carouselItems?.[idx]);
-          }}
-        >
-          {carouselItems &&
-            carouselItems?.map((b, idx) => (
-              <Banner
-                key={`banner-${idx + 1}`}
-                backgroundColor={b?.backgroundColor}
-              >
-                <img loading="lazy" src={b.imageUrl} />
-                <BannerOverlay backgroundColor={b?.backgroundColor}>
-                  <div className="banner-content">
-                    <div className="banner-text">
-                      <div className="banner-title">{b.title}</div>
-                      {b.description}
-                    </div>
-                    {isDefinedNotEmptyString(b?.buttonText) && (
-                      <Button
-                        color="neutral"
-                        // trailingIcon="arrow-right"
-                        className="banner-action"
-                        onClick={(e) => {
-                          e?.stopPropagation();
+  ({ items = [], onSelectItem = () => {}, onButtonAction = () => {} }, ref) => {
+    const responsive = {
+      desktop: {
+        breakpoint: { max: 3000, min: 1024 },
+        items: 1,
+        partialVisibilityGutter: 0,
+      },
+      tablet: {
+        breakpoint: { max: 1024, min: 464 },
+        items: 1,
+        partialVisibilityGutter: 0,
+      },
+      mobile: {
+        breakpoint: { max: 464, min: 0 },
+        items: 1,
+        partialVisibilityGutter: 48,
+      },
+    };
 
-                          onButtonAction(b);
-                        }}
-                      >
-                        {b.buttonText}
-                      </Button>
-                    )}
-                  </div>
-                </BannerOverlay>
-              </Banner>
-            ))}
+    const isMobile = useDetectMobile();
+
+    return (
+      <Container key={`banner-section-carousel__${isMobile}`} ref={ref}>
+        <Carousel
+          responsive={responsive}
+          removeArrowOnDeviceType="mobile"
+          infinite={!isMobile}
+          keyBoardControl={true}
+          autoPlay={true}
+          partialVisible={true}
+          sliderClass="carousel-slider"
+          itemClass="carousel-item"
+          containerClass="carousel-container"
+        >
+          {items?.map((item, idx) => (
+            <div
+              key={`banner-section-carousel-item__${idx + 1}`}
+              className="section__card"
+              onClick={() => onSelectItem(item)}
+            >
+              <img
+                src={item?.imageUrl}
+                alt={`Slide ${idx + 1}`}
+                className="section__image"
+              />
+              <div className="card__content">
+                <div className="card__text">
+                  {item?.title && (
+                    <div className="card__title">{item?.title}</div>
+                  )}
+                  {item?.title && (
+                    <div className="card__description">{item?.description}</div>
+                  )}
+                </div>
+                {item?.buttonText && (
+                  <Button
+                    text={item?.buttonText}
+                    className="card__action"
+                    size="medium"
+                    onClick={(e) => {
+                      e?.stopPropagation();
+
+                      onButtonAction(item);
+                    }}
+                  />
+                )}
+              </div>
+            </div>
+          ))}
         </Carousel>
-      </Wrapper>
+      </Container>
     );
   }
 );
 
 BannerSectionCarousel.propTypes = {
   items: PropTypes.array,
-  handleClick: PropTypes.func,
+  onSelectItem: PropTypes.func,
+  onButtonAction: PropTypes.func,
 };
 
 export default BannerSectionCarousel;
--- src/Landing Sections/banners-sections/banner-section-carousel/style.jsx
+++ src/Landing Sections/banners-sections/banner-section-carousel/style.jsx
@@ -1,177 +1,146 @@
 import styled from "@emotion/styled";
-import { MOBILE_SIZE_PX } from "../../../_utils/consts";
-import { isDefinedNotEmptyString } from "../../../_utils/utils";
 
-export const Wrapper = styled.div`
-  position: relative;
+export const Container = styled.div`
+  &:hover {
+    cursor: pointer;
 
-  & .image-carousel {
-    height: 20.625rem;
-
-    & img {
-      height: 20.625rem;
-    }
-
-    & .next,
-    .previous {
-      display: none;
+    & .react-multiple-carousel__arrow {
+      visibility: visible;
     }
   }
 
-  @media (max-width: ${MOBILE_SIZE_PX + "px"}) {
-    & .image-carousel {
-      & > div {
-        border-radius: unset;
-      }
-      & img {
-        border-radius: unset;
-      }
-    }
-  }
-`;
-
-export const Block = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  background: ${(p) =>
-    isDefinedNotEmptyString(p?.backgroundColor)
-      ? p?.backgroundColor
-      : `linear-gradient(
-    to top left,
-    rgba(0, 0, 0, 0) 85%,
-    rgba(0, 0, 0, 1) 100%
-  )`};
-
-  border-radius: 0%.75rem;
-
-  width: 100%;
-  height: 100%;
-  padding: 3rem 3rem 5.625rem 3rem;
-
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-  justify-content: space-between;
-
-  & button {
-    width: fit-content;
+  & .carousel-slider {
+    align-items: center;
   }
 
-  & .text-area {
-    max-width: 31.75rem;
-    font-size: 1rem;
-    display: flex;
-    flex-direction: column;
-    gap: 0.5rem;
-    color: var(--white);
-
-    & .title {
-      font-weight: 700;
-      font-size: 2rem;
-      color: var(--white, #fff);
-    }
+  & .carousel-container {
+    padding-bottom: 1.25rem;
   }
 
-  @media (max-width: ${MOBILE_SIZE_PX + "px"}) {
-    display: none;
-  }
-`;
-
-export const Banner = styled.div`
-  position: relative;
-  border-radius: 0.75rem;
-  cursor: pointer;
-
-  background: ${(p) =>
-    isDefinedNotEmptyString(p?.backgroundColor)
-      ? p?.backgroundColor
-      : `linear-gradient(
-    to top left,
-    rgba(0, 0, 0, 0) 85%,
-    rgba(0, 0, 0, 1) 100%
-  )`};
-
-  & img {
-    object-fit: cover;
-    max-height: 20.625rem;
-    border-radius: 0.75rem;
-    mix-blend-mode: multiply;
+  & .carousel-item {
+    padding-right: 0;
   }
 
-  & .banner-action {
-    background: var(--white, #fff);
-    color: var(--gray-950, #14161a);
-    font-size: 0.875rem;
-    font-style: normal;
-    font-weight: 500;
-    line-height: 1.25rem;
+  & .react-multiple-carousel__arrow {
+    width: 3rem;
+    height: 3rem;
+    padding: 0.5rem;
+    background: rgba(255, 255, 255, 0.8);
+    box-shadow: 1px 1px 4px 0 rgba(20, 22, 26, 0.08),
+      0 2px 16px 0 rgba(20, 22, 26, 0.16);
+    backdrop-filter: blur(1.25rem);
+    visibility: hidden;
 
-    &:hover {
-      background: var(--gray-200, #dddfe4);
+    &::before {
       color: var(--gray-950, #14161a);
     }
-  }
 
-  @media (max-width: ${MOBILE_SIZE_PX + "px"}) {
-    border-radius: unset;
-
-    & img {
-      border-radius: unset;
+    &:hover {
+      background: rgba(255, 255, 255, 0.5);
     }
   }
-`;
 
-export const BannerOverlay = styled.div`
-  border-radius: 0.75rem;
-  position: absolute;
-  top: 0;
-  left: 0;
-  height: 100%;
-  width: 100%;
-  padding: 3rem 0 3rem 3rem;
-
-  background: ${(p) =>
-    isDefinedNotEmptyString(p?.backgroundColor)
-      ? p?.backgroundColor
-      : `linear-gradient(
-    to top left,
-    rgba(0, 0, 0, 0) 85%,
-    rgba(0, 0, 0, 1) 100%
-  )`};
-
-  & .banner-content {
-    display: flex;
-    flex-direction: column;
-    gap: 3.5rem;
-    width: 45%;
+  & .react-multiple-carousel__arrow--left {
+    left: 1rem;
   }
 
-  & .banner-content button {
-    width: fit-content;
+  & .react-multiple-carousel__arrow--right {
+    right: 1rem;
   }
 
-  & .banner-text {
-    text-align: left;
-    font-size: 1rem;
+  & .section__card {
     display: flex;
     flex-direction: column;
-    gap: 0.5rem;
-    color: var(--white, #fff);
+    justify-content: space-between;
+    align-items: flex-start;
+    flex: 1 0 0;
+    align-self: stretch;
+    border-radius: 0.75rem;
+    width: 100%;
+    height: 22.5rem;
+    position: relative;
+    overflow: hidden;
 
-    & .banner-title {
-      font-weight: 700;
-      font-size: 2rem;
+    & .card__content {
+      position: absolute;
+      top: 0;
+      left: 0;
+      padding: 3rem;
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+    }
+
+    & .card__text {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      gap: 0.25rem;
+    }
+
+    & .card__title {
       color: var(--white, #fff);
+      font-size: 1.75rem;
+      font-style: normal;
+      font-weight: 600;
+      line-height: 2.25rem;
+    }
+
+    & .card__description {
+      color: var(--white, #fff);
+      font-size: 1rem;
+      font-style: normal;
+      font-weight: 400;
+      line-height: 1.5rem;
+    }
+
+    & .card__action {
+      padding: 0.625rem 0.75rem;
+      background: var(--white, #fff);
+      color: var(--gray-950, #14161a);
+      font-size: 0.875rem;
+      font-style: normal;
+      font-weight: 500;
+      line-height: 1.25rem;
+      width: fit-content;
     }
   }
 
-  @media (max-width: ${MOBILE_SIZE_PX + "px"}) {
-    border-radius: unset;
-    background: unset;
+  & .react-multi-carousel-item:not(.react-multi-carousel-item--active) {
+    & .section__card {
+      height: 22.5rem;
+    }
+  }
 
-    & .banner-content {
-      display: none;
+  & .section__image {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    display: block;
+  }
+
+  @media (max-width: 768px) {
+    & .section__card {
+      height: 23.4375rem;
+      border-radius: 0.5rem;
+
+      & .card__content {
+        display: none;
+      }
+    }
+
+    & .react-multi-carousel-item:not(.react-multi-carousel-item--active) {
+      & .section__card {
+        height: 20.625rem;
+        transform: scale(0.95);
+      }
+    }
+  }
+
+  @media (max-width: 1024px) {
+    & .carousel-item {
+      padding-right: 0.5rem;
     }
   }
 `;
