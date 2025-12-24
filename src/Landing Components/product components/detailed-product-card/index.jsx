6004ff5fe87471ca0131a7028c41d7a9bd314b33
Amar Hasanovic <amar.hasanovic@lanaco.com>
2025-12-24 14:28:20 +0100

    background

--- src/Landing Components/product components/detailed-product-card/index.jsx
+++ src/Landing Components/product components/detailed-product-card/index.jsx
@@ -57,7 +57,7 @@ const DetailedProductCard = forwardRef((props, ref) => {
     bookmarked,
     bookmarkLists,
     metadata,
-    sellerName
+    sellerName,
   } = props;
 
   const isMobile = useDetectMobile();
