f9f58501dc8d602039b015c0ad66637811a1e27c
Milan Brčkalo <brckvlo3@gmail.com>
2026-01-12 10:35:16 +0100

    Removed unused func

--- src/Landing Components/skeleton-components/shop/shop-cards/style.jsx
+++ src/Landing Components/skeleton-components/shop/shop-cards/style.jsx
@@ -1,12 +1,10 @@
 import styled from "@emotion/styled";
 
 import { MOBILE_SIZE_PX } from "../../../../_utils/consts";
-import { skeletonGradientBackground, SkeletonRowWrapper } from "../../style";
+import { SkeletonRowWrapper } from "../../style";
 
 export const Container = styled(SkeletonRowWrapper)`
   @media (max-width: ${MOBILE_SIZE_PX + "px"}) {
-    flex-direction: column;
-
     & > div {
       background: linear-gradient(
         to right,
